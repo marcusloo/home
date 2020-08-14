3edeacd100adc4ead83329fc69fed0463d509494
mloo3 <mloo3@gatech.edu>
2020-08-14 02:26:13 -0400

    Updates

---
+++ precache-manifest.823d70fe1a47a8c7cc99c161b57c7333.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "848eddf35dc9960b2a3af8f55f15e2d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c666181a5b148c47560",
+    "url": "/static/css/main.f549acf8.chunk.css"
+  },
+  {
+    "revision": "b370a36a54baf515216a",
+    "url": "/static/js/2.80bbdedd.chunk.js"
+  },
+  {
+    "revision": "9450e89efc00352ad8dfbbdbfb1ef77a",
+    "url": "/static/js/2.80bbdedd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c666181a5b148c47560",
+    "url": "/static/js/main.e6a2a6c1.chunk.js"
+  },
+  {
+    "revision": "ba126735810163618457",
+    "url": "/static/js/runtime-main.3374ac37.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.1758ed933d816d66b830d8a6fb431a25.js"
+  "/precache-manifest.823d70fe1a47a8c7cc99c161b57c7333.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/home/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
